8d8244ed47cdd711d69e2741d3b4d1eb7ef7ae1b
jerry12122 <a0975844268@gmail.com>
2021-07-22 13:55:46 +0800

    Signed-off-by: jerry12122 <a0975844268@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
+function onImageLoaded(url, cb) {
+	var image = new Image()
+	image.src = url
+	if (image.complete) {
+		cb(image)
+	} else {
+		image.onload = function () {
+		cb(image)
+		}
+	}
+}
+
 $(function(){
-	//一段正则，匹配所有_min.的图片src属性
 	var test = /_min\./
-	//遍历所有的图片节点
 	$("img").each(function(index,obj){	
 		if(test.test($(this).attr("src"))){
 			var reSrc = $(this).attr("src").replace(test,".");
@@ -12,15 +22,15 @@ $(function(){
 			image.src = reSrc
 		}		
 	})
+
     $("body").each(function(index,obj){	
 		if(test.test($(this).css('background-image'))){
+			var tmp = $(this)
 			var reSrc = $(this).css('background-image').replace(test,".");
 			reSrc = reSrc.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
-			var image = new Image()
-			if(image.complete){
-				$(this).css('background-image', 'url(' + reSrc + ')');
-			} 
-			image.src = reSrc
+			onImageLoaded(reSrc,function(icon){
+				tmp.css('background-image', 'url(' + reSrc + ')');
+			})
 		}		
 	})
 
