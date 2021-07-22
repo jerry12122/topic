0353ab5c7a0c4897e6a0269405591a6202877440
jerry12122 <a0975844268@gmail.com>
2021-07-22 10:28:48 +0800

    Signed-off-by: jerry12122 <a0975844268@gmail.com>

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(function(){
+	//一段正则，匹配所有_min.的图片src属性
+	var test = /_min\./
+	//遍历所有的图片节点
+	$("img").each(function(index,obj){	
+		if(test.test($(this).attr("src"))){
+			var reSrc = $(this).attr("src").replace(test,".");
+			$(this).attr("src",reSrc)
+		}		
+	})
+    
+    $("body").each(function(index,obj){	
+		if(test.test($(this).css('background-image'))){
+			var reSrc = $(this).css('background-image').replace(test,".");
+			$(this).css('background-image',reSrc)
+		}		
+	})
+
+})
